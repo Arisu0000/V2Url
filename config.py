7e8448e45e7c766287b822d577e8a3b69306467d
Pullaiah <130484247+Pullaiah9701@users.noreply.github.com>
2024-06-24 16:46:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,35 +18,38 @@ API_ID = int(os.environ.get("API_ID", "24025974"))
 API_HASH = os.environ.get("API_HASH", "2abf0406f41a57b540bdefe8b12d114f")
 # Bot token from @BotFather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6809564656:AAFzMXyJ5Bqc7k4XjbCNfGPSj20RQhyUQzw")
+# Admin IDs
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "6290483448").split(",") if i.strip().isdigit()]
 
-# Admins list
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "6290483448").split(",") if i.strip()] if os.environ.get("ADMINS") else []
-OWNER_ID = int(os.environ.get("OWNER_ID", "5549620776"))  # ID of the owner
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "UrlShortx24")
+# MongoDB URI from https://www.mongodb.com/
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL", "mongodb+srv://vasanthtg8:vasanthtg8@cluster0.441zwwt.mongodb.net/?retryWrites=true&w=majority"
+)
+# Owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "5549620776"))
 if OWNER_ID not in ADMINS:
     ADMINS.append(OWNER_ID)
 
-# Database settings
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "UrlShortx24")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://vasanthtg8:vasanthtg8@cluster0.441zwwt.mongodb.net/?retryWrites=true&w=majority")  # MongoDB URI
-
 # Optional variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001725631702"))  # Log channel for information about users
-UPDATE_CHANNEL = int(os.environ.get("UPDATE_CHANNEL", "-1001409325739"))  # For force subscription
-BROADCAST_AS_COPY = is_enabled(os.environ.get("BROADCAST_AS_COPY", "True"), False)  # True if forwarding should be avoided
-IS_PRIVATE = is_enabled(os.environ.get("IS_PRIVATE", "False"), False)  # True for private use and restricting users
-SOURCE_CODE = os.environ.get("SOURCE_CODE")  # For upstream repo
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001725631702"))
+# For Force Subscription
+UPDATE_CHANNEL = int(os.environ.get("UPDATE_CHANNEL", "-1001409325739"))
+BROADCAST_AS_COPY = is_enabled(os.environ.get("BROADCAST_AS_COPY", "True"), False)
+IS_PRIVATE = is_enabled(os.environ.get("IS_PRIVATE", "False"), False)
+SOURCE_CODE = os.environ.get("SOURCE_CODE")
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "https://graph.org/file/557eb3ed747c867c775d5.jpg")
-LINK_BYPASS = is_enabled(os.environ.get("LINK_BYPASS", "False"), False)  # If true, URLs will be bypassed
-BASE_SITE = os.environ.get("BASE_SITE", "")  # Your shortener site domain
+LINK_BYPASS = is_enabled(os.environ.get("LINK_BYPASS", "False"), False)
+BASE_SITE = os.environ.get("BASE_SITE", "")
 
 # For Admin use
 CHANNELS = is_enabled(os.environ.get("CHANNELS", "True"), True)
-CHANNEL_ID = [int(i.strip()) for i in os.environ.get("CHANNEL_ID", "").split(" ") if i.strip()] if os.environ.get("CHANNEL_ID") else []
-DE_BYPASS = [i.strip() for i in os.environ.get("DE_BYPASS", "").split(",") if i.strip()] if os.environ.get("DE_BYPASS") else []
-if "mdisk.me" not in DE_BYPASS:
-    DE_BYPASS.append("mdisk.me")
+CHANNEL_ID = [int(i.strip()) for i in os.environ.get("CHANNEL_ID", "").split(" ") if i.strip().isdigit()]
 
-FORWARD_MESSAGE = is_enabled(os.environ.get("FORWARD_MESSAGE", "False"), False)  # True if forward message to be converted by reposting the post
+DE_BYPASS = [i.strip() for i in os.environ.get("DE_BYPASS", "").split(",") if i.strip()]
+DE_BYPASS.append("mdisk.me")
+
+FORWARD_MESSAGE = is_enabled(os.environ.get("FORWARD_MESSAGE", "False"), False)
 
 WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "True"), True)
 PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "240"))
